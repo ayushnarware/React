4929e65bf477891aec026d3accbdaa1b2b68c0c7
Ayush Narware <152053797+ayushnarware@users.noreply.github.com>
2025-11-13 20:40:37 +0530

    react app

---
+++ app/src/Pages/hero.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import About from "../components/About.jsx";
+import Contact from "../components/Contact.jsx";
+import Home from "../components/Home.jsx";
+
+const HomePage = () => {
+  
+  return (
+    <div>
+      <Home />
+      <About />
+      <Contact />
+    </div>
+  );
+};
+
+export default hero;
