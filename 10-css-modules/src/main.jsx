fcb1b0b17e1093864d1ebc9b3d90e49bc19cffed
Auto Deploy Script <actions@github.com>
2025-11-16 20:58:20 +0530

    chore: deploy built outputs 2025-11-16 20:58:19Z

--- 10-css-modules/src/main.jsx
+++ 10-css-modules/src/main.jsx
@@ -1,11 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // ✅ Import added
+import App from './App.jsx';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <h1 className="heading">This is our app</h1>
-    <App />
-  </StrictMode>,
-)
+  <BrowserRouter>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </BrowserRouter>
+);
